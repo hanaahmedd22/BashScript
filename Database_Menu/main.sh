94046c1a04ff74339674f8ce1d78d4515857bdf3
hanaahmedd <hana_meky@yahoo.com>
2025-03-22 00:53:33 +0200

    feat: intializing folders  with entry files for the 2 menus

---
+++ Database_Menu/main.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+while true; do
+    choice=$(zenity --list --title="Bash DBMS" --column="Options" \
+        "Create Database" "List Databases" "Connect to Database" "Drop Database" "Exit" \
+        --height=300 --width=400)
+
+    case $choice in
+        "Create Database")
+            ./create_db.sh
+            ;;
+        "List Databases")
+            ./list_dbs.sh
+            ;;
+        "Connect to Database")
+            ./connect_db.sh
+            ;;
+        "Drop Database")
+            ./drop_db.sh
+            ;;
+        "Exit")
+            exit 0
+            ;;
+        *)
+            zenity --error --text="Invalid choice. Please try again."
+            ;;
+    esac
+done
