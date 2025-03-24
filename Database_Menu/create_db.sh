82b7c0d9db767162ae03445ca9e897671a56f738
toka shawky <tokaelmaadawy@gmail.com>
2025-03-24 23:09:52 +0200

    fix: create database in a separate folder

--- Database_Menu/create_db.sh
+++ Database_Menu/create_db.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
+if [[ ! -d "../Databases" ]]; then
+    mkdir "../Databases"
+fi
+
 if [[ -z "$1" ]]; then
     read -p "Enter database name: " db_name
 else
     db_name="$1"
 fi
-source ./helper/validate_db_name.sh 
+
+source ./helper/validate_db_name.sh
 validate_db_name "$db_name" || exit 1
-if mkdir "./$db_name"; then
+
+
+if mkdir "../Databases/$db_name"; then
     echo "Database '$db_name' created successfully."
 else
     echo "Error: Failed to create database."
