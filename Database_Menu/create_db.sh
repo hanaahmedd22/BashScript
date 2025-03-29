f7664a300dab322adcdef10d8e2a7dc2928364b7
TOOKAA <toka@localhost.localdomain>
2025-03-29 01:13:11 -0400

    fix: Handle case when user presses OK or Cancel in create_db

--- Database_Menu/create_db.sh
+++ Database_Menu/create_db.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-source ./helper/validate_db_name.sh
+source ../helper/validate_db_name.sh
 while true; do
-    db_name=$(zenity --entry --text="Please Enter a Database name:")
-    [[ -z "$db_name" ]] && zenity --info --text="Operation cancelled by the user." && exit 1
-
-    if validate_db_name "$db_name"; then
-        break
-    fi
+    db_name=$(zenity --entry --text="Please Enter a Database name:" --height=50 --width=300)
+    [[ $? -ne 0 ]] && zenity --info --text="Operation cancelled by the user." && ./main.sh && exit 1
+    validate_db_name "$db_name" && break
 done
 
 if mkdir "../Databases/$db_name"; then
