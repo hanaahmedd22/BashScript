825f89c9456482bf15eed06e1c9d0218cbcaca57
hanaahmedd <hana_meky@yahoo.com>
2025-03-29 04:15:17 +0200

    fix:create table in connected database (checking number of columns)

--- Database_Connection_Menu/create_table.sh
+++ Database_Connection_Menu/create_table.sh
@@ -17,18 +17,18 @@ done
 data_file="$db_path/${table_name}.data"
 meta_data_file="$db_path/${table_name}.meta"
 
+while true;do
+    num_col=$(zenity --entry --title="Number of Columns" --text="Enter Number of Columns:" --width=300)
 
-num_col=$(zenity --entry --title="Number of Columns" --text="Enter Number of Columns:" --width=300)
-
-if ! [[ "$num_col" =~ ^[0-9]+$ ]] || [ "$num_col" -le 0 ]; then
-    zenity --error --text="Invalid number of columns. Please enter a positive integer." --width=300
-    exit 1
-elif [ -z "$num_col" ]; then
-    zenity --error --text="Number of columns cannot be empty." --width=300
-    exit 1
-
-fi
-
+    if ! [[ "$num_col" =~ ^[0-9]+$ ]] || [ "$num_col" -le 0 ]; then
+        zenity --error --text="Invalid number of columns. Please enter a positive integer." --width=300
+        continue
+    elif [ -z "$num_col" ]; then
+        zenity --error --text="Number of columns cannot be empty." --width=300
+        continue
+    fi
+    break
+done
 column=()
 data_type=()
 
