16a3bafca7fdd9788f83121a954283635cb43aec
hanaahmedd <hana_meky@yahoo.com>
2025-03-30 14:58:53 +0200

    fix:editing validation functions for create table

--- helper/validate_create_table.sh
+++ helper/validate_create_table.sh
@@ -4,35 +4,37 @@ CheckIfValidOfTableName() {
     input=$1
     db_path=$2
 
-    if [ -f "$db_path/$table_name*" ]; then
-        echo "Table already exists"
-        zenity --error --text="Table '$table_name' already exists."
-        return 1 
-    elif [[ $input =~ ^[a-zA-Z]+[0-9]*$ ]]; then
-        input=$(echo "$input" | tr ' ' '_')
-	    return 0
-    elif [ -z $input ];then
-	    echo "your input is empty"
+    if [ -z "$input" ]; then
+        echo "Your input is empty"
         zenity --error --text="Table name cannot be empty."
-	    return 1
-    else
-        echo "Invalid input try again"
-        zenity --error --text="Invalid table name. Please use only letters and numbers, starting with a letter."
-	    return 1
-        
+        return 1
     fi
+
+    if ! [[ "$input" =~ ^[a-zA-Z][a-zA-Z0-9_]*$ ]]; then
+        echo "Invalid input, try again"
+        zenity --error --text="Invalid table name. Please use only letters, numbers, and underscores, starting with a letter.Without spaces."
+        return 1
+    fi
+
+    
+    if ls "$db_path/$input"* >/dev/null 2>&1; then
+        echo "Table already exists"
+        zenity --error --text="Table '$input' already exists."
+        return 1
+    fi
+    return 0
 }
+
 
 CheckIfValidOfName() {
     input=$1
-    if [[ $input =~ ^[a-zA-Z]+[0-9]*$ ]]; then
-        input=$(echo "$input" | tr ' ' '_')
+    if [[ $input =~ ^[a-zA-Z][a-zA-Z0-9_]*$]]; then
 	    return 0
     elif [ -z $input ];then
 	echo "your input is empty"
 	    return 1
     else
-        echo "Invalid input try again"
+        echo "Invalid column name. Please use only letters and numbers, starting with a letter.Without spaces"
 	    return 1
         
     fi
