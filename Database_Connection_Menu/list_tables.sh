db679c5d479a1ace289d2dfd6b12af0064f04a5c
hanaahmedd <hana_meky@yahoo.com>
2025-03-30 16:17:44 +0200

    fix : in list table script

--- Database_Connection_Menu/list_tables.sh
+++ Database_Connection_Menu/list_tables.sh
@@ -5,11 +5,19 @@ db_path=$2
 tables=$(ls "$db_path" | grep ".meta$" | sed 's/.meta$//')
 
 if [ -z "$tables" ]; then
-    zenity --info --text="No tables found in the database path: $db_path" --width=300
+    zenity --info --text="No tables found in the database path: $db_path" --width=300  
+    ../Database_Connection_Menu/main.sh "$db_name"
     exit 0
-fi
 
-zenity --list --title="Available Tables" \
-       --text="Tables in the database named: $db_name" \
-       --column="Tables" $tables \
-       --width=400 --height=300
+else
+
+    zenity --list --title="Available Tables" \
+        --text="Tables in the database named: $db_name" \
+        --column="Tables" $tables \
+        --width=400 --height=300
+    status=$?
+    if [ "$status" -ne 0 ]; then
+        ../Database_Connection_Menu/main.sh "$db_name"
+        exit 0
+    fi
+fi
